826cd1a483c66c821a7c8b1318159f63f7e726f9
Your Name <you@example.com>
2022-10-05 19:29:17 +0300

    pusku

--- Player.gd
+++ Player.gd
@@ -1,14 +1,13 @@
 extends KinematicBody
 
 var run_speed : float = 8.0
+
 var time : float = 0.0
 var step_freq : float = 2.0 #kuinka usein
 var step_height : float = 0.2 #kuinka korkealle maasta
 var step_tilt : float = 8.0 # degress: asteet
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
+onready var body_hinge = $BodyHinge
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,4 +19,26 @@ func _ready():
 #	pass
 
 func _physics_process(delta):
-	move_and_slide(Vector3(0,0,-run_speed))
+	body_hinge.translation.y = step_height * (1 + sin(2.0 * PI * step_freq * time))
+	body_hinge.rotation_degrees.z = step_tilt * sin(PI * step_freq *time)
+	time += delta
+	
+	var sideways : float = 0.0
+	
+	if Input.is_action_pressed("move_right"):
+		sideways += 1.0
+	if Input.is_action_pressed("move_left"):
+		sideways -= 1.0 
+	
+	move_and_slide(Vector3(sideways * run_speed, 0 ,-run_speed))
+
+
+
+
+
+
+
+
+
+
+
